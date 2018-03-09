a04a6478b67de8ccfe80980da789919bab9f9d22
tickbg <tickbg@gmail.com>
2018-03-09 17:43:42 +0200

    Added a naming library for parsing media filenames

--- movie_scanner.py
+++ movie_scanner.py
@@ -4,15 +4,6 @@ import re
 
 movie_db_api_key = 'aff5c411d31602d0f54a97b4b677d524'
 
-clean_date_time_expr = \
-    re.compile('(.+[^ _\,\.\(\)\[\]\-])[ _\.\(\)\[\]\-]+(19[0-9][0-9]|20[0-1][0-9])([ _\,\.\(\)\[\]\-][^0-9]|$)')
-
-clean_string_expr = (
-    re.compile('[ _\,\.\(\)\[\]\-](ac3|dts|custom|dc|divx|divx5|dsr|dsrip|dutch|dvd|dvdrip|dvdscr|dvdscreener|screener|dvdivx|cam|fragment|fs|hdtv|hdrip|hdtvrip|internal|limited|multisubs|ntsc|ogg|ogm|pal|pdtv|proper|repack|rerip|retail|cd[1-9]|r3|r5|bd5|se|svcd|swedish|german|read.nfo|nfofix|unrated|ws|telesync|ts|telecine|tc|brrip|bdrip|480p|480i|576p|576i|720p|720i|1080p|1080i|2160p|hrhd|hrhdtv|hddvd|bluray|x264|h264|xvid|xvidvd|xxx|www.www|\[.*\])([ _\,\.\(\)\[\]\-]|$)'),
-    re.compile('[ _\,\.\(\)\[\]\-](3d|sbs|tab|hsbs|htab|mvc|\[.*\])([ _\,\.\(\)\[\]\-]|$)'),
-    re.compile('(\[.*\])')
-)
-
 
 def is_media_file(filepath):
     pass
---
+++ naming/expressions.py
@@ -0,0 +1,69 @@
+import re
+
+explanation_words = (
+    'ac3','dts','custom','dc','divx','divx5','dsr','dsrip','dutch','dvd','dvdrip','dvdscr',
+    'dvdscreener','screener','dvdivx','cam','fragment','fs','hdtv','hdrip','hdtvrip','internal',
+    'limited','multisubs','ntsc','ogg','ogm','pal','pdtv','proper','repack','rerip','retail',
+    'cd[1-9]','r3','r5','bd5','se','svcd','swedish','german','read.nfo','nfofix','unrated','ws',
+    'telesync','ts','telecine','tc','brrip','bdrip','480p','480i','576p','576i','720p','720i','1080p',
+    '1080i','2160p','hrhd','hrhdtv','hddvd','bluray','x264','h264','xvid','xvidvd','xxx','www.www'
+)
+
+clean_string_expr = (
+    re.compile(r'[ _\,\.\(\)\[\]\-]' + '(' + r'|'.join(explanation_words) + r'|\[.*\])' + r'([ _\,\.\(\)\[\]\-]|$)'),
+    re.compile('[ _\,\.\(\)\[\]\-](3d|sbs|tab|hsbs|htab|mvc|\[.*\])([ _\,\.\(\)\[\]\-]|$)'),
+    re.compile('(\[.*\])')
+)
+
+video_file_stack_expr = (
+    re.compile(r'(.*?)([ _.-]*(?:cd|dvd|p(?:ar)?t|dis[ck])[ _.-]*[0-9]+)(.*?)(\\.[^.]+)$'),
+    re.compile(r'(.*?)([ _.-]*(?:cd|dvd|p(?:ar)?t|dis[ck])[ _.-]*[a-d])(.*?)(\\.[^.]+)$'),
+    re.compile(r'(.*?)([ ._-]*[a-d])(.*?)(\\.[^.]+)$')
+)
+
+year_expr = re.compile(r'(.+[^ _\,\.\(\)\[\]\-])'\
+                        '[ _\.\(\)\[\]\-]+'\
+                        '(19[0-9][0-9]|20[0-1][0-9])'\
+                        '([ _\,\.\(\)\[\]\-][^0-9]|$)')
+
+video_file_extensions = (
+    '.m4v', '.3gp','.nsv','.ts','.ty','.strm','.rm','.rmvb','.m3u','.ifo','.mov','.qt','.divx','.xvid','.bivx',
+    '.vob','.nrg','.img','.iso','.pva','.wmv','.asf','.asx','.ogm','.m2v','.avi','.bin','.dat','.dvr-ms',
+    '.mpg','.mpeg','.mp4','.mkv','.avc','.vp3','.svq3','.nuv','.viv','.dv','.fli','.flv','.rar','.001','.wpl','.zip'
+)
+
+audio_file_extensions = (
+    '.nsv','.m4a','.flac','.aac','.strm','.pls','.rm','.mpa','.wav',
+    '.wma','.ogg','.opus','.mp3','.mp2','.m3u','.mod','.amf','.669','.dmf','.dsm','.far','.gdm','.imf','.it',
+    '.m15','.med','.okt','.s3m','.stm','.sfx','.ult','.uni','.xm','.sid','.ac3','.dts','.cue','.aif',
+    '.aiff','.wpl','.ape','.mac','.mpc','.mp+','.mpp','.shn','.zip','.rar','.wv','.nsf','.spc','.gym',
+    '.adplug','.adx','.dsp','.adp','.ymf','.ast','.afc','.hps','.xsp','.acc','.m4b','.oga','.dsf','.mka'
+)
+
+subtitle_file_extensions = ('.srt', '.ssa', '.ass', '.sub')
+
+album_stacking_prefixes = ('disc', 'cd', 'disk', 'vol', 'volume')
+
+
+class ExpessionError(Exception):
+    """ expression generic error. """
+    pass
+
+
+def clean_year(path):
+    match = re.search(year_expr, path)
+    try:
+        name, year = match.group(1), int(match.group(2))
+        return name, year
+    except Exception as err:
+        raise ExpessionError('Error matching name or date %s' % str(err))
+
+
+def clean_string(path):
+    for expr in expressions.clean_string_expr:
+        try:
+            found = re.search(expr, path)
+            clean_path = path[0:path.find(found.group(1))]
+            return clean_path
+        except Exception as err:
+            raise ExpessionError('Error clean string %s' % str(err))
