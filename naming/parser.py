e0e890274341e0d4c262c79a75bc2799c1e29e07
emo <tickbg@gmail.com>
2018-04-10 17:17:22 +0300

    Implemented video stack parser function

--- naming/parser.py
+++ naming/parser.py
@@ -1,43 +1,43 @@
 import os
 import re
-import expressions as expr
+import expressions
 
 
 def parse_video_stub_type(path):
     # Check for stub type (dvd, bluray ..)
     filename, ext = os.path.splitext(path)
-    if ext in expr.stub_extensions:
+    if ext in expressions.stub_extensions:
         stbext = os.path.splitext(filename)[1].strip('.')
-        for token in iter(expr.video_stub_types):
+        for token in iter(expressions.video_stub_types):
             if token == stbext:
-                return expr.video_stub_types[token]
+                return expressions.video_stub_types[token]
     return None
 
 
 def parse_video_extra_type(path):
     # Check for extra type (trailer or sample)
     filename, ext = os.path.split(path)
-    for re_token in iter(expr.video_extra_types):
+    for re_token in iter(expressions.video_extra_types):
         if re.search(re_token, filename):
-            return expr.video_extra_types[re_token]
+            return expressions.video_extra_types[re_token]
     return None
 
 
 def parse_video_3d_type(path):
     # Check if video is 3D
     filename, ext = os.path.split(path)
-    for token in expr.video_3d_types:
+    for token in expressions.video_3d_types:
         if filename.find(token):
-            return expr.video_3d_types[token]
+            return expressions.video_3d_types[token]
     return None
 
 
 def is_video_file(path):
-    return lower(os.path.splitext(path)[1]) in expr.video_extensions
+    return os.path.splitext(path)[1].lower() in expressions.video_extensions
 
 
 def is_stub_file(path):
-    return lower(os.path.splitext(path)[1]) in expr.stub_extensions
+    return os.path.splitext(path)[1].lower() in expressions.stub_extensions
 
 
 def parse_video(path):
@@ -47,15 +47,15 @@ def parse_video(path):
         return None
     filename, ext = os.path.splitext(path)
     stubtype = None
-    if ext not in expr.video_extensions:
+    if ext not in expressions.video_extensions:
         stubtype = parse_video_stub_type(path)
     container = ext.strip('.')
-    name, year = expr.clean_year(path)
+    name, year = expressions.clean_year(path)
     format3D = parse_video_stub_type(path)
     xtype = parse_video_extra_type(path)
     # Do a second pas to extract name and year after cleaning the path string
     if not name or not year:
-        name, year = expr.clean_year(expr.clean_string(path))
+        name, year = expressions.clean_year(expressions.clean_string(path))
     name = os.path.split(name)[1]
     return {
         'name'      : name,
@@ -69,39 +69,56 @@ def parse_video(path):
 
 def parse_video_stack(files):
     # Directories are skiped
-    filelist = [f for f in files if not os.path.isdir(f)
-                  and (is_video_file(f) or is_stub_file(f))].sort()
-    stack_result = None
-    for fname in filelist:
-        fidx = 1
-        for rexpr in expr.video_file_stack_expr:
-            match = re.search(rexpr, fname)
-            match_next = re.search(rexpr, filelist[fidx])
-            if not match or match_next:
+    filelist = [f for f in files if is_video_file(f) or is_stub_file(f)]
+    filelist.sort()
+    stacks = []
+    fidx_offset = 0
+    stack_expressions = expressions.video_file_stack_expr
+    for fidx in range(len(filelist)):
+        fname = filelist[fidx]
+        if fidx_offset:
+            fidx = fidx_offset - 1
+        expr_index = 0
+        while expr_index < len(stack_expressions):
+            file_stack = {
+                'name'  : None,
+                'files' : []
+            }
+            expr = stack_expressions[expr_index]
+            match = re.search(expr, fname)
+            if not match:
+                expr_index += 1
                 continue
-            title = match.group(1)
-            volume = match.group(2)
-            ignore = match.group(3)
-            title_next = match_next.group(1)
-            volume_next = match_next.group(2)
-            ignore_next = match_next.group(3)
-            if title == title_next \
+            fnext = (fidx + fidx_offset - 1) + 1
+            while fnext < len(filelist):
+                stack_files = []
+                match_next = re.search(expr, filelist[fnext])
+                if not match_next:
+                    break
+                title = match.group(1)
+                volume = match.group(2)
+                ignore = match.group(3)
+                title_next = match_next.group(1)
+                volume_next = match_next.group(2)
+                ignore_next = match_next.group(3)
+
+                if title == title_next \
                     and volume != volume_next \
-                    and ignore == ignore_next:
-                if os.path.splitext(fname) == os.path.splitext(filelist[fidx]):
-                    stack_files = []
-                    if not stack_result:
-                        stack_files.append(fname)
-                        stack_result = {
-                            'name' : title,
-                            'files' : stack_files
-                        }
-                    stack_files.append(filelist[idx])
-            else:
-                continue
+                    and ignore == ignore_next \
+                    and os.path.splitext(fname)[1] == os.path.splitext(filelist[fnext])[1]:
 
+                    if not (file_stack['name']):
+                        file_stack['name'] = title
+                        file_stack['files'].append(fname)
+                    file_stack['files'].append(filelist[fnext])
+                fnext += 1
 
-
+            if len(file_stack['files']):
+                stacks.append(file_stack)
+                fidx_offset += len(file_stack['files']) - 1
+                break
+            expr_index += 1
+    return stacks
 
 
 def parse_video_list(files):
--- naming/test_parser.py
+++ naming/test_parser.py
@@ -15,5 +15,33 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(video_info['container'], 'mkv')
         self.assertEqual(video_info['year'], 2006)
 
+    def test_parse_video_stack(self):
+        files = [
+            'Bad Boys (2006) part1.mkv',
+            'Bad Boys (2006) part2.mkv',
+            'Bad Boys (2006) part3.mkv',
+            'Bad Boys (2006) part4.mkv',
+            'Bad Boys (2006)-trailer.mkv',
+        ]
+        stack = parser.parse_video_stack(files)
+        self.assertEqual(len(stack), 1)
+
+    def test_parse_dual_video_stacks(self):
+        files = [
+            'Bad Boys (2006) part1.mkv',
+            'Bad Boys (2006) part2.mkv',
+            'Bad Boys (2006) part3.mkv',
+            'Bad Boys (2006) part4.mkv',
+            'Bad Boys (2006)-trailer.mkv',
+            '300 (2006) part1.mkv',
+            '300 (2006) part2.mkv',
+            '300 (2006) part3.mkv',
+            '300 (2006)-trailer.mkv'
+        ]
+        stack = parser.parse_video_stack(files)
+        print(stack)
+        self.assertEqual(len(stack), 2)
+
+
 if __name__ == '__main__':
     unittest.main()
