1ee373784143c0b6a76dfff5f5589da7605cf754
emo <tickbg@gmail.com>
2018-06-14 18:11:41 +0300

    Added logic for loading channels

--- api_manager.py
+++ api_manager.py
@@ -7,6 +7,10 @@ class ApiManager:
         self._chmngr = self._app['channels']
         self._libmngr = self._app['libraries']
 
+    async def handle_libraries(self, request):
+        response_text = self._libmngr.get_libraries()
+        return web.Response(text=response_text)
+
     async def handle_channels(self, request):
         response_text = self._chmngr.get_channels()
         return web.Response(text=response_text)
@@ -16,11 +20,12 @@ class ApiManager:
         return web.Response(text=response_text)
 
     def setup_routes(self):
-        # self._app.router.add_static('/', self.handle_view)
-        # self._app.router.add_static('/ui', self.handle_view)
+        self._app.router.add_get('/', self.handle_view)
+        self._app.router.add_get('/ui', self.handle_view)
         self._app.router.add_get('/channels', self.handle_channels)
+        self._app.router.add_get('/libraries', self.handle_libraries)
+        # Disabled for now, but will be part of the future api
         '''
-        self._app.router.add_get('/libraries')
         self._app.router.add_get('/users')
         self._app.router.add_get(r'/users/{id}')
         self._app.router.add_get(r'/channels/{id}')
@@ -33,3 +38,4 @@ class ApiManager:
         self._app.router.add_get(r'/devices')
         self._app.router.add_get(r'/devices/{id}')
         '''
+
