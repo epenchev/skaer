4b4799021be2857b2b1e6f8e47706f1bc3ecdec9
emo <tickbg@gmail.com>
2018-08-17 17:46:46 +0300

    Remove the short menu

--- ui_example/index.js
+++ ui_example/index.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $('.leftmenutrigger').on('click', function (e) {
     $('.side-nav').toggleClass("open");
     $('#wrapper').toggleClass("open");
+    $('.side-nav').toggleClass("d-none");
     e.preventDefault();
   });
 });
