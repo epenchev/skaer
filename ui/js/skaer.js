a01718b07d8086e32c6c4ecb2a93f4e53939e461
Emil Penchev <emo@clearcorrect.com>
2018-10-12 17:29:42 +0300

    UI work in progress minor changes

--- ui/js/skaer.js
+++ ui/js/skaer.js
@@ -1,44 +1,28 @@
 $(function () {
     var mediaCollections = []
-    // $('.view-panel').removeClass('visible');
-
     $.getJSON( "media_collections.json", function( data ) {
         mediaCollections = data;
         generateMediaCollectionsHTML(mediaCollections);
         renderViewPanel(mediaCollections);
-        // trace
-        console.log('download media collections done');
     });
 
     function renderViewPanel(data) {
-        var viewPanel = $('.view-panel'),
-            collections = $('.view-panel .collections-list > li');
-
-        // Hide all collections
-        // collections.addClass('hidden');
-
         // Iterate over all of the collections
         // If their ID is somewhere in the data object remove the hidden class to reveal them.
         collections.each(function () {
 		    var that = $(this);
 
             data.forEach(function (item) {
-                console.log(item.id);
-                console.log(that.data('index'));
-                console.log("\n");
-                if(that.data('index') == item.id) {
+                if( that.data('index') == item.id) {
                     that.removeClass('hidden');
-                    console.log('remove')
                 }
             });
         });
 
         // Show the view-panel
         // (the render function hides all pages so we need to show the one we want).
-        viewPanel.addClass('visible');
-        collections = $('.view-panel .collections-list');
-        collections.addClass('visible');
-        console.log(collections.html());
+        $('.view-panel').addClass('visible');
+        $('.collections-list').addClass('visible');
     }
 
 	function generateMediaCollectionsHTML(data) {
